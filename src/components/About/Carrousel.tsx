10d4c1a04287c992aaa3354d60c1ec2a2a597518
Martin Angulo <martinangulo1194@gmail.com>
2022-10-21 15:52:34 -0500

    casi termiando

--- src/components/About/Carrousel.tsx
+++ src/components/About/Carrousel.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
-import styles from "./Carrousel.module.css";
+import React, { useContext, useState } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import useDrag from "./useDrag";
+import styles from "./Carrousel.module.css";
+import "./hideScrollbar.css";
+
+
+const getItems = () =>
+  Array(50)
+    .fill(0)
+    .map((_, ind) => ({ id: ind + 1 }));
+
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
+
+function LeftArrow() {
+  const { isFirstItemVisible, scrollPrev } = useContext(VisibilityContext);
+  console.log(useContext(VisibilityContext).visibleItems);
+  return (
+    <button className={isFirstItemVisible ? styles.btn_left_disable : styles.btn_left} disabled={isFirstItemVisible} onClick={() => scrollPrev()}>
+    </button>
+  );
+}
+
+function RightArrow() {
+  const { isLastItemVisible, scrollNext } = useContext(VisibilityContext);
+  return (
+    <button className={isLastItemVisible ? styles.btn_right_disable : styles.btn_right} disabled={isLastItemVisible} onClick={() => scrollNext()}>
+    </button>
+  );
+}
+
 
 export default function Carrousel() {
+
+  const [items, setItems] = useState(getItems);
+
+  const { dragStart, dragStop, dragMove, dragging } = useDrag();
+  const handleDrag = ({ scrollContainer }: scrollVisibilityApiType) => (
+    ev: React.MouseEvent
+  ) =>
+    dragMove(ev, (posDiff) => {
+      if (scrollContainer.current) {
+        scrollContainer.current.scrollLeft += posDiff;
+      }
+    });
+
+  const handleItemClick = (itemId: string) => () => {
+    if (dragging) {
+      return false;
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <button className={styles.btn_left}>{"<"}</button>
-      <img className={styles.imgs}></img>
-      <button className={styles.btn_right}>{">"}</button>
+      <div onMouseLeave={dragStop}>
+        <ScrollMenu
+          LeftArrow={LeftArrow}
+          RightArrow={RightArrow}
+          onMouseDown={() => dragStart}
+          onMouseUp={() => dragStop}
+          onMouseMove={handleDrag}
+        >
+          {items.map(({ id }) => (
+            <img
+              className={styles.img}
+              draggable={false}
+              onClick={() => handleItemClick("item-" + id)}
+              src={require(`../../img/carrousel/img-${id}.jpg`)}
+              id={"item-" + id}
+            />
+          ))}
+        </ScrollMenu>
+      </div>
     </div>
   )
 }
+
+
